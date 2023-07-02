74d85b84e572891b372dc0304947c90dca93b21e
Ashrith <ashrithmr01@gmail.com>
2023-07-02 17:32:45 +0530

    contents-home-socials

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,24 @@
 const navMenu=document.getElementById("nav-menu"),
     navToggle=document.getElementById("nav-toggle"),
     navClose=document.getElementById("nav-close")
-
+// Show Menu
 if(navToggle){
     navToggle.addEventListener("click",()=>{
         navMenu.classList.add("show-menu");
         navToggle.style.display="none";
     })
 }
+// Close Menu
+if(navClose){
+    navClose.addEventListener("click",()=>{
+        navMenu.classList.remove("show-menu");
+        navToggle.style.display="inline-block"
+    })
+}
+
+const navLink=document.querySelectorAll(".nav_link")
+function linkAction(){
+    navMenu.classList.remove('show-menu')
+    navToggle.style.display="inline-block"
+}
+navLink.forEach(n=>n.addEventListener('click',linkAction))
